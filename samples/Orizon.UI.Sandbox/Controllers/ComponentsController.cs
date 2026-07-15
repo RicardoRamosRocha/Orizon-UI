f2fe34b1ca230ef90be6aefa097e4ce44e2dd207
Ricardo Ramos da Rocha <ricardo.32ramos@gmail.com>
2026-07-15 19:46:35 +0000

    feat(ui): finalize MVP foundation and stabilize sandbox

---
+++ samples/Orizon.UI.Sandbox/Controllers/ComponentsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Orizon.UI.Sandbox.Controllers;
+
+[Route("components")]
+public sealed class ComponentsController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("buttons")]
+    public IActionResult Buttons()
+    {
+        return View();
+    }
+
+    [HttpGet("cards")]
+    public IActionResult Cards()
+    {
+        return View();
+    }
+
+    [HttpGet("alerts")]
+    public IActionResult Alerts()
+    {
+        return View();
+    }
+
+    [HttpGet("badges")]
+    public IActionResult Badges()
+    {
+        return View();
+    }
+}
